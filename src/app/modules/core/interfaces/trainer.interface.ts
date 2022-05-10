245088300cf69b60169919abbe3b67f648531a58
DiegoCeccon <dceccon.dextra@alelo.com.br>
2022-05-10 09:47:32 -0300

    feat: ✨ Introduce first interface of trainer

---
+++ src/app/modules/core/interfaces/trainer.interface.ts
@@ -0,0 +1,4 @@
+export interface Trainer {
+  nickname: string;
+  email: string;
+}
--- src/app/modules/trainer/controllers/trainer.controller.ts
+++ src/app/modules/trainer/controllers/trainer.controller.ts
@@ -1,11 +1,16 @@
 import { Request, Response } from 'express';
+import { Trainer } from '../../core/interfaces/trainer.interface';
 
 export const getTrainers = (req: Request, res: Response) => {
   res.send('Get Well done Trainer!');
 };
 
 export const getTrainerById = (req: Request, res: Response) => {
-  res.send('Get Well done Trainer by id!');
+  const trainer: Trainer = {
+    nickname: 'Diguya',
+    email: 'diguya@poketeam.com'
+  };
+  res.send(trainer);
 };
 
 export const putTrainer = (req: Request, res: Response) => {
