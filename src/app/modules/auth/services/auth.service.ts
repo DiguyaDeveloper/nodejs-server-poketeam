2b21cdd80a61966ee5f1c63ce888b4715077c965
DiegoCeccon <dceccon.dextra@alelo.com.br>
2022-05-10 09:28:43 -0300

    feat: ✨ Update auth module routes

--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -1,13 +1,9 @@
 import { Request, Response } from 'express';
 
-export const getTrainerById = (req: Request, res: Response) => {
-  res.send('Get Well done Trainer by id!');
+export const signin = (req: Request, res: Response) => {
+  res.send('Signin Well done!');
 };
 
-export const putTrainer = (req: Request, res: Response) => {
-  res.send('Put Well done Trainer by id!');
-};
-
-export const postTrainer = (req: Request, res: Response) => {
-  res.send('Post Well done Trainer!');
+export const signup = (req: Request, res: Response) => {
+  res.send('Signup Well done!');
 };
--- src/app/routes/auth.ts
+++ src/app/routes/auth.ts
@@ -1,17 +1,11 @@
 import { Router } from 'express';
-import {
-  getTrainerById,
-  postTrainer,
-  putTrainer
-} from '../modules/auth/controllers/auth.controller';
+import { signin, signup } from '../modules/auth/controllers/auth.controller';
 
 const router = Router();
 
-// Create Trainer
-router.post('', postTrainer);
-// Get Trainer by id
-router.get('/:id', getTrainerById);
-// Update Trainer by id
-router.put('/:id', putTrainer);
+// Create Trainer User
+router.post('signin', signin);
+// Get Trainer User
+router.post('signup', signup);
 
 export default router;
